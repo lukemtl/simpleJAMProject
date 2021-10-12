e53ad7cd352a7a1e4fc83279c1bcf4b0ef175fd6
Luke Kumar <lukekumar@Lukes-iMac.local>
2021-10-11 20:13:14 -0400

    add comments placeholder

--- temp.js
+++ temp.js
@@ -9,3 +9,11 @@
    //        do first commit           ( commit -m "init commit")
    //        do remote add to set up link with git server (copy code from site)
    //        push to git server ( push -u origin master)
+
+// GITHUB navigate to settings => github pages, make the source for your site the master branch
+
+// DEPLOYING UPDATE
+   // index.html add placeholder for comments
+   // add all changes for next commit (git add .)
+   // do commit ( commit -m "add placeholder for comments")
+   // push to server ( push )
