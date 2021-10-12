9df15fe93c9c17fe03201e694aed9588777480d0
Luke Kumar <lukekumar@Lukes-iMac.local>
2021-10-11 20:08:37 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# the most basic jamstack project
---
+++ temp.js
@@ -0,0 +1,11 @@
+// create basic HTML file with boilerplate, h1 / paragraph... link to stylesheet...
+
+// README.md => # The most basic jamstack project
+
+// GITHUB create repository, copy the remote add origin command from site
+
+// TERMINAL: initialize the project as a git repo    (init)
+   //        have the whole directory watched by git   ( add . )
+   //        do first commit           ( commit -m "init commit")
+   //        do remote add to set up link with git server (copy code from site)
+   //        push to git server ( push -u origin master)
